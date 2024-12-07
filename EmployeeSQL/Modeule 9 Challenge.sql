9e974f12c49f9ab10becb61e3374a4c6e3f80a8b
Tucker Morris <tuckermorris@Tuckers-MBP.lan>
2024-12-07 14:12:57 -0800

    adding sql schema and query

---
+++ EmployeeSQL/Modeule 9 Challenge.sql
@@ -0,0 +1,85 @@
+CREATE TABLE if not exists departments (
+    dept_no VARCHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE if not exists dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(4),
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+CREATE TABLE if not exists dept_manager (
+    dept_no VARCHAR(4),
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE if not exists Employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id INT,
+    birth_date DATE,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex CHAR(1),
+    hire_date DATE
+);
+
+CREATE TABLE if not exists salaries (
+    emp_no INT,
+    salary DECIMAL(10, 2),
+    PRIMARY KEY (emp_no, salary),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE if not exists titles (
+    title_id INT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date::text LIKE '1986%';
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
